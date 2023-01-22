2264521a44edc02db5d6b9459bd10ea4f969a563
Gary Grant <garygrant693@gmail.com>
2023-01-22 02:42:59 -0500

    created video background

--- portfolio/src/Component/UI/Hero/Hero.js
+++ portfolio/src/Component/UI/Hero/Hero.js
@@ -1,8 +1,12 @@
 import styles from './Hero.module.css';
+import backgroundVideo from '../../../videos/inkInWater.mp4';
+
 
 function Hero(props){
     return(
-       <div></div>
+       <video className={styles.video} autoPlay muted loop>
+        <source src={backgroundVideo} type='video/mp4'/>
+       </video>
     )
 }
 
