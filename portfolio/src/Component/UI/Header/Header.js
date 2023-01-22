cf875571af5b3dd58863f7d7f0c6051927b7615c
Gary Grant <garygrant693@gmail.com>
2023-01-22 01:46:54 -0500

    created first components for portfolio site

---
+++ portfolio/src/Component/UI/Header/Header.js
@@ -0,0 +1,11 @@
+import styles from  './Header.module.css';
+
+function Header(props){
+    return (
+        <div>
+
+        </div>        
+    )
+}
+
+export default Header
---
+++ portfolio/src/Component/UI/Hero/Hero.js
@@ -0,0 +1,10 @@
+import styles from './Hero.module.css';
+
+function Hero(props){
+    return(
+       <div></div>
+    )
+}
+
+export default Hero;
+
