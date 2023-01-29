2ad2b6d689e74c03b46f36e335646befafc4d465
Gary Grant <garygrant693@gmail.com>
2023-01-28 22:28:32 -0500

    added react router

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,22 @@
-import { Fragment } from 'react';
+import React from 'react';
+import { Route,Switch} from 'react-router-dom';
 import Home from './Pages/Home';
+import About from './Pages/AboutMe'; 
+import Projects from './Pages/Projects';
+
 function App() {
   return (
-    <Fragment >
-      <main>
-      <Home/>
-      </main>
-    </Fragment>
+    <Switch>
+      <Route  path='/'>
+        <Home/>
+     </Route>
+     <Route path='/projects'>
+       <Projects/>
+     </Route>
+     <Route path='/About'>
+      <About/>
+     </Route>
+    </Switch>
   );
 }
 
