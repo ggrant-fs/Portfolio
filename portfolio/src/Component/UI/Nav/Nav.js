ce8f5da07fdd49aef7ae2ee6e75d004e0f9d7e51
Gary Grant <garygrant693@gmail.com>
2023-02-07 12:22:03 -0500

    convertied the app to sass with a 7 - 1 architecture

--- portfolio/src/Component/UI/Nav/Nav.js
+++ portfolio/src/Component/UI/Nav/Nav.js
@@ -1,37 +1,38 @@
 import React from 'react';
 import { Fragment } from 'react';
 import {Link} from 'react-router-dom';
-import styles from './Nav.module.css';
+import '../../../index.css';
+// import styles from './Nav.module.css';
 
 const Nav=(props)=> {
    return(
      <Fragment>
-        <div className={styles.fullpageNav}>
-            <ul className={styles.navlist}>
-                <li className={styles.navItem}>
-                  <Link to="#" activeClassName={styles.active}>
+        <div className={"fullpageNav"}>
+            <ul className={"navlist"}>
+                <li className={"navItem"}>
+                  <Link to="#" activeClassName={"active"}>
                     <h4>Featured Work</h4>
                   </Link>
                 </li>
-                <li className={styles.navItem}>
-                  <Link to="/projects" activeClassName={styles.active}>
+                <li className={"navItem"}>
+                  <Link to="/projects" activeClassName={"active"}>
                     <h4>Portfolio</h4>
                   </Link>
                 </li>
-                <li className={styles.navItem}>
-                  <Link to="/about" activeClassName={styles.active}>
+                <li className={"navItem"}>
+                  <Link to="/about" activeClassName={"active"}>
                     <h4>About Me</h4>
                   </Link>
                 </li>
             </ul>
-            <div className={styles.socialLinksContainer}>
-              <ul className={styles.socalLinks}>
-                <li className={styles.socalLink}>
+            <div className={"socialLinksContainer"}>
+              <ul className={"socalLinks"}>
+                <li className={"socalLink"}>
                   <Link to={{pathname:"/https://github.com/ggrant-fs"}} target="_blank">
                   <span>LinkedIn</span>
                   </Link>
                 </li>
-                <li className={styles.socalLink}>
+                <li className={"socalLink"}>
                   <Link to={{pathname:"/https://github.com/ggrant-fs"}} target="_blank">
                     <snan>GitHub</snan>
                  </Link>
