9dae3878a5fea4ad141f761dcc1ef1ebc25bdcd9
Gary Grant <garygrant693@gmail.com>
2023-06-17 23:19:17 -0400

    added the boilerplate for Projects.js

--- portfolio/src/Component/UI/Projects/Projects.js
+++ portfolio/src/Component/UI/Projects/Projects.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Projects =(props)=>{
     return(
         <div>
-
+           
         </div>
     )
 }
--- portfolio/src/Pages/Projects.js
+++ portfolio/src/Pages/Projects.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Fragment } from 'react';
 
 function Projects (props){
     return (
-        <div>
-            
-        </div>
+        <Fragment>
+            <div>
+                <div className="page-title">Lastest Work</div>
+                <div className="portfolio-imgs">
+                    <div className="portfolio-imgs__1">
+                        <img className='project-img'src='https://i.imgur.com/vDQBpeo.png' alt=''/>
+                    </div>
+                    <div className="portfolio-imgs__2">
+                        <img className='project-img' src='https://i.imgur.com/dNouR6Y.png' atl=''/>
+                    </div>
+                </div>
+            </div>
+        </Fragment>
     )
 }
 
