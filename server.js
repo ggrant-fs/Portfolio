9c330760b8c45d03657c274b1fb6e0583c040bed
Gary Grant <garygrant693@gmail.com>
2023-06-22 21:27:18 -0400

    added Heroku deployment code

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const path = require('node:path');
+
 const app = express();
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('portfolio/build'));
+
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'portfolio','build','index.html'));
+    })
+}
 
 const port = process.env.PORT || 8080;
 
