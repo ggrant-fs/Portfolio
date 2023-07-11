21dc0ab6547dcffe1a44ad4f8b1aab2b95093bf7
Gary Grant <garygrant693@gmail.com>
2023-07-10 22:20:29 -0400

    added back button to header

--- portfolio/src/Pages/AboutMe.js
+++ portfolio/src/Pages/AboutMe.js
@@ -1,18 +1,20 @@
 import React from 'react';
+import {useHistory} from 'react-router-dom';
 import { Fragment } from 'react';
 
 function AboutMe(props){
+    const history = useHistory();
     return(
         <Fragment>
             <div>
                 <header className="aboutMe-header">
-                      
+                  <button className='aboutMe__back__btn' onClick={()=> history.goBack()}>Back</button>
                 </header>
                 <div className='aboutMe-img-wrapper'>
 
                 </div>
                 <div className='aboutMe-content'>
-                    
+
                 </div>
             </div>
         </Fragment>
