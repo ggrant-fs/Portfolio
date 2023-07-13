8cd63176bf06dfc1d65cb679d9bd99aaec48e637
Gary Grant <garygrant693@gmail.com>
2023-07-12 22:39:09 -0400

    added flexbox to .aboutMe__content

--- portfolio/src/Pages/AboutMe.js
+++ portfolio/src/Pages/AboutMe.js
@@ -10,11 +10,19 @@ function AboutMe(props){
                 <header className="aboutMe-header">
                   <button className='aboutMe__back__btn' onClick={()=> history.goBack()}>Back</button>
                 </header>
-                <div className='aboutMe-img-wrapper'>
-
-                </div>
-                <div className='aboutMe-content'>
-
+                <div className="aboutMe-container">
+                   <div className='aboutMe__img'></div>
+                   <div className='space-between'></div>
+                   <div className='aboutMe__content'>
+                   <div className='space-between'></div>
+                    <h1 className='aboutMe__title'>Gary Grant</h1>
+                    <h2 className='aboutMe__subtitle'>Software Engineer</h2>
+                    <div className='aboutMe__details'>
+                        <p>
+                            lorem impsum   
+                        </p>
+                    </div>
+                   </div>
                 </div>
             </div>
         </Fragment>
