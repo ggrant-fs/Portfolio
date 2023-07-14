d0caeb36dc75b94d85df83f49efffeb30fe671fb
Gary Grant <garygrant693@gmail.com>
2023-07-13 23:34:30 -0400

    added a paragraph and font styling

--- portfolio/src/Pages/AboutMe.js
+++ portfolio/src/Pages/AboutMe.js
@@ -19,7 +19,12 @@ function AboutMe(props){
                     <h2 className='aboutMe__subtitle'>Software Engineer</h2>
                     <div className='aboutMe__details'>
                         <p>
-                            lorem impsum   
+                         Welcome to my portfolio, I'm Gary Grant. I'm a developer
+                         that loves taking ideas and concepts and bringing them to 
+                         life.I began my career in finance helping individuals with
+                         their financial needs but my passion has lead me to assist 
+                         others with their engineering goals. Feel free to check out 
+                         my latest projects and reach out to me via Linkedin.
                         </p>
                     </div>
                    </div>
