02f78ff36472d2dccc94a37945b7ff19a0f44d95
Gary Grant <garygrant693@gmail.com>
2023-09-14 21:39:49 -0400

    new build

---
+++ portfolio/build/precache-manifest.38fc70912cc5811c294f8677ca8c7c6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b8961ddee4dd9bb94c1",
+    "url": "/static/css/main.af7f8658.chunk.css"
+  },
+  {
+    "revision": "6b8961ddee4dd9bb94c1",
+    "url": "/static/js/main.fde0be3c.chunk.js"
+  },
+  {
+    "revision": "ad834586ed3382ddf2d7",
+    "url": "/static/js/runtime~main.bf02eaaa.js"
+  },
+  {
+    "revision": "b569b2acdb22b42c2ac5",
+    "url": "/static/js/2.8438ff96.chunk.js"
+  },
+  {
+    "revision": "4af07b562c0602e03473",
+    "url": "/static/js/3.a772fac8.chunk.js"
+  },
+  {
+    "revision": "b254881608544affacff2d39d39f4a1c",
+    "url": "/static/media/inkinwater.b2548816.mp4"
+  },
+  {
+    "revision": "c95d6cef48dda046653fc1b6fa07303b",
+    "url": "/index.html"
+  }
+];
